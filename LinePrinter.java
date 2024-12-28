26cbac5c4a554768f95df8e8007d2a5aaa05cbdd
Yury Pazharski <Pozharskiyuri@gmail.com>
2024-12-28 19:36:21 +0300

    Add LinePrinter class with printSameTextLines method

---
+++ LinePrinter.java
@@ -0,0 +1,7 @@
+public class LinePrinter {
+    public static void printSameTextLines(String text, int linesQuantity) {
+        for (int i = 0; i < linesQuantity; i++) {
+            System.out.println(text);
+        }
+    }
+}
