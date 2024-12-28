1befe5d60b442c5166860c5abd666e266de1cf2f
Yury Pazharski <Pozharskiyuri@gmail.com>
2024-12-28 19:15:12 +0300

    Add NumberSignChecker class with printNumberSignInfo method

---
+++ NumberSignChecker.java
@@ -0,0 +1,5 @@
+public class NumberSignChecker {
+    public static void printNumberSignInfo(int number) {
+        System.out.println(number < 0 ? "Число отрицательное" : "Число положительное");
+    }
+}
