ee486bc0b084f3f3a1002be6b6805a8b76db2dec
Yury Pazharski <Pozharskiyuri@gmail.com>
2024-12-30 14:17:45 +0300

    Change logic of isLeapYear according to Gregorian calendar

--- Lesson_7/BitSwitcher.java
+++ Lesson_7/BitSwitcher.java
@@ -3,7 +3,7 @@ package Lesson_7;
 public class BitSwitcher {
 
     /**
-     * Switches the bit array
+     * Switches the bit array. If null in the parameter no exception will be thrown.
      *
      * @param bits array of bits (0 or 1 only) that are to be switched
      */
--- Lesson_7/YearChecker.java
+++ Lesson_7/YearChecker.java
@@ -2,8 +2,15 @@ package Lesson_7;
 
 public class YearChecker {
 
+    /**
+     * @param year of Gregorian calendar
+     * @return true if the year is leap according to Gregorian calendar, otherwise - false
+     */
     public static boolean isLeapYear(int year) {
-        year = Math.abs(year);
+        if (year < 1584) {
+            return false;
+        }
+
         return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 
