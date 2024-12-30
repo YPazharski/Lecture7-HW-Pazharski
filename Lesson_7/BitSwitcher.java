2e0b95bdd586168af22c10c98b050eb899471ed1
Yury Pazharski <Pozharskiyuri@gmail.com>
2024-12-30 14:38:28 +0300

    Add array elements validation in switchBites method.
    The method throws IllegalArgumentException if one or more of array elements are not equal to 0 nor 1.

--- Lesson_7/BitSwitcher.java
+++ Lesson_7/BitSwitcher.java
@@ -7,8 +7,16 @@ public class BitSwitcher {
      *
      * @param bits array of bits (0 or 1 only) that are to be switched
      */
-    public static void switchBites(byte[] bits) {
+    public static void switchBites(byte[] bits) throws IllegalArgumentException {
         if (bits != null) {
+            for (int i = 0; i < bits.length; i++) {
+                bits[i] ^= 1;
+                if (bits[i] != 0 && bits[i] != 1) {
+                    throw new IllegalArgumentException("the element on the " + i + " position is " + bits[i] +
+                            ". It must be 0 or 1");
+                }
+            }
+
             for (int i = 0; i < bits.length; i++) {
                 bits[i] ^= 1;
             }
