7a3c3782baa32e938d7b0f4748561e1d30b01f67
Yury Pazharski <Pozharskiyuri@gmail.com>
2024-12-31 21:46:41 +0300

    Add IntArrayCreator tests

--- Lesson_7/IntArrayCreator.java
+++ Lesson_7/IntArrayCreator.java
@@ -1,5 +1,7 @@
 package Lesson_7;
 
+//6. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
+
 public class IntArrayCreator {
 
     public static int[] createIntArray(int numberOfElements) throws IllegalArgumentException {
