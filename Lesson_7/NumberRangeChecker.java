877cbe7ddeda28a19f50d6e90ac8e6119923cf67
Yury Pazharski <Pozharskiyuri@gmail.com>
2024-12-31 16:31:44 +0300

    Add task description to NumberRangeChecker class file

--- Lesson_7/NumberRangeChecker.java
+++ Lesson_7/NumberRangeChecker.java
@@ -1,5 +1,8 @@
 package Lesson_7;
 
+//1. Написать метод, принимающий на вход два целых числа и проверяющий,
+// что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
+
 public class NumberRangeChecker {
 
     public static boolean isNumbersSumBetweenTenAndTwenty(int firstNumber, int secondNumber) {
