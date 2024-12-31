c6c290553d6e29ad4808c2417d3175bc5320da82
Yury Pazharski <Pozharskiyuri@gmail.com>
2024-12-31 21:22:03 +0300

    Fix switchBites bug, add BitSwitcher tests

--- Lesson_7/BitSwitcher.java
+++ Lesson_7/BitSwitcher.java
@@ -1,5 +1,8 @@
 package Lesson_7;
 
+//5. Задать целочисленный массив, состоящий из элементов 0 и 1.
+// Например: [1, 1, 0, 0, 1, 0, 1, 1, 0, 0]. С помощью цикла и условия заменить 0 на 1, 1 на 0;
+
 public class BitSwitcher {
 
     /**
@@ -10,7 +13,6 @@ public class BitSwitcher {
     public static void switchBites(byte[] bits) throws IllegalArgumentException {
         if (bits != null) {
             for (int i = 0; i < bits.length; i++) {
-                bits[i] ^= 1;
                 if (bits[i] != 0 && bits[i] != 1) {
                     throw new IllegalArgumentException("the element on the " + i + " position is " + bits[i] +
                             ". It must be 0 or 1");
